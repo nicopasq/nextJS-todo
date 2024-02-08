d23841c922ba958e8db2a0540f4210682c2a58f5
nicopasq <90681300+nicopasq@users.noreply.github.com>
2024-02-08 14:37:54 -0700

    Added fonts and buttons

---
+++ app/ui/fonts.ts
@@ -0,0 +1,17 @@
+import { Nabla } from "next/font/google";
+import { Bungee_Spice } from "next/font/google";
+import { Pacifico } from "next/font/google";
+
+export const nabla = Nabla({
+    subsets:['latin']
+})
+
+export const bungee_spice = Bungee_Spice({
+    subsets:['latin'],
+    weight:['400']
+})
+
+export const pacifico = Pacifico({
+    subsets:['latin'],
+    weight:['400']
+})
